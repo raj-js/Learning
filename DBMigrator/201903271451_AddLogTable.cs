292749ce7b2e57edf58e9ffea579c2de7efcfad8
xueguang.zhan <xueguang.zhan@macrowing.com>
2019-03-27 15:10:44 +0800

    基本迁移

---
+++ DBMigrator/201903271451_AddLogTable.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace DBMigrator
+{
+    [Migration(201903271451)]
+    public class _201903271451_AddLogTable : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Log");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Log")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Text").AsString()
+                .WithColumn("CreateTime").AsDateTime();
+        }
+    }
+}
---
+++ DBMigrator/Program.cs
@@ -0,0 +1,42 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DBMigrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = CreateServices();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddMySql5()
+                    .WithGlobalConnectionString("Server=127.0.0.1;Port=3306;Database=migrator; User=root;Password=1qaz2WSX;")
+                    .ScanIn(typeof(_201903271451_AddLogTable).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+    }
+}
