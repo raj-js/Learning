d8c47368689ed804869c409a7b90841c425ab819
xueguang.zhan <xueguang.zhan@macrowing.com>
2019-04-28 18:12:57 +0800

    将 Context 改为 Rainbow 支持

---
+++ LearningPolly/LearningPolly/LearningFallback.cs
@@ -0,0 +1,15 @@
+﻿using Polly;
+using System;
+
+namespace LearningPolly
+{
+    class LearningFallback
+    {
+        public static void Fallback()
+        {
+            //Policy
+            //   .Handle<Exception>()
+            //   .Fallback()
+        }
+    }
+}
