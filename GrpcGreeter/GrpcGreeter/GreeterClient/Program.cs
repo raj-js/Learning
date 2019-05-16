50b17c73aa10041d39ccf97c76cea3accc651c8c
xueguang.zhan <xueguang.zhan@macrowing.com>
2019-05-16 20:34:31 +0800

    gRPC 初尝

---
+++ GrpcGreeter/GrpcGreeter/GreeterClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Google.Protobuf.Collections;
+using Greet;
+using Grpc.Core;
+using Sample;
+
+namespace GreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = new Channel("localhost:50051", ChannelCredentials.Insecure);
+
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest {Name = "Raj"});
+            Console.WriteLine($"Greeting: {reply.Message}");
+
+            var sampleClient = new Simple.SimpleClient(channel);
+            var simpleReply = await sampleClient.DoSimpleThingAsync(new SimpleRequest {Arg = "Raj"});
+            Console.WriteLine($"simple reply: {simpleReply.D}");
+
+            var otherReq = new OtherReq
+            {
+                A = "1",
+                B = 2,
+                C = false
+            };
+            otherReq.D.Add("r1");
+            otherReq.D.Add("r2");
+            otherReq.D.Add("r3");
+            var otherReply = await sampleClient.DoOtherThingAsync(otherReq);
+            Console.WriteLine($"other reply: {otherReply.Code}, {otherReply.Msg}, {otherReply.Data}");
+
+            await channel.ShutdownAsync();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GrpcGreeter/GrpcGreeter/GrpcGreeter/Services/SampleService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.JSInterop;
+using Sample;
+
+namespace GrpcGreeter
+{
+    public class SampleService : Simple.SimpleBase
+    {
+        public override Task<SimpleResponse> DoSimpleThing(SimpleRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new SimpleResponse { D = $"do simple thing with {request.Arg}" });
+        }
+
+        public override Task<OtherResp> DoOtherThing(OtherReq request, ServerCallContext context)
+        {
+            return Task.FromResult(new OtherResp
+            {
+               Code = 0,
+               Msg =  "operation succeed",
+               Data = Json.Serialize(request)
+            });
+        }
+    }
+}
