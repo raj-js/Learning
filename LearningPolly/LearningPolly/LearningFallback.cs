57728387c471c95c0f3887044b5f53fba50a5cda
xueguang.zhan <xueguang.zhan@macrowing.com>
2019-08-15 14:23:39 +0800

    多层降级

--- LearningPolly/LearningPolly/LearningFallback.cs
+++ LearningPolly/LearningPolly/LearningFallback.cs
@@ -1,35 +1,77 @@
 ﻿using Polly;
 using System;
-using System.Threading.Tasks;
 
 namespace LearningPolly
 {
     class LearningFallback
     {
+        private const string PolicyKey = "policy_key";
+
+        private static Policy Resilience =>
+           Policy.Wrap(
+
+               Policy
+                .Handle<Exception>()
+                .WaitAndRetry(5, t => TimeSpan.FromSeconds(1)),
+
+               Policy
+                .Handle<Exception>()
+                .CircuitBreaker(3, TimeSpan.FromSeconds(10),
+                    (e, s, t, c) =>
+                    {
+                        Console.WriteLine("break");
+                    },
+                    c =>
+                    {
+                        Console.WriteLine("reset");
+                    },
+                    () =>
+                    {
+                        Console.WriteLine("half open");
+                    }
+                )
+               );
+
+        private static Policy<T> CreatePolicy<T>(Policy<T> fallback, Func<T> previous)
+        {
+            return Policy<T>
+                .Handle<Exception>()
+                .Fallback(
+                    () =>
+                        fallback == null ?
+                            previous() :
+                            fallback.Execute(previous)
+                )
+                .Wrap(Resilience);
+        }
+
         public static void Try()
         {
-            var avatar = Policy<UserAvatar>
-                .Handle<Exception>()
-                .FallbackAsync(
-                
-                // 降级处理
-                async (res, c, cancellationToken) => 
-                {
-                    return await Task<UserAvatar>.Factory.StartNew(() => 
-                    {
-                        return UserAvatar.Blank;
-                    }, cancellationToken);
-                }, 
+            Policy<UserAvatar> policy = null;
 
-                // 失败时调用
-                (res, c) => 
+            policy = CreatePolicy(
+                policy, previous: () =>
                 {
-                    Console.WriteLine($"onFallbackAsync called:{res?.Exception?.Message}");
-                    return Task.CompletedTask;
-                })
-                .ExecuteAsync(async () => await Task.FromResult(UserAvatar.FromReal));
+                    Console.WriteLine("in memory");
+                    return UserAvatar.InMemory;
+                }
+            );
 
-            Console.WriteLine($"Get User Avatar: {avatar.Result?.Url}");
+            policy = CreatePolicy(
+                policy, previous: () =>
+                {
+                    Console.WriteLine("mysql");
+                    return UserAvatar.MySql;
+                }
+            );
+
+            var avatar = policy.Execute(() =>
+            {
+                Console.WriteLine("redis");
+                return UserAvatar.Redis;
+            });
+
+            Console.WriteLine(avatar.Url);
         }
     }
 
@@ -37,8 +79,10 @@ namespace LearningPolly
     {
         public string Url { get; private set; }
 
-        public static UserAvatar Blank => new UserAvatar { Url = "default.png" };
+        public static UserAvatar Redis => throw new Exception("Server Error");
 
-        public static UserAvatar FromReal => throw new Exception("Server Error");
+        public static UserAvatar MySql => throw new Exception("Server Error");
+
+        public static UserAvatar InMemory => new UserAvatar { Url = "default.png" };
     }
 }
